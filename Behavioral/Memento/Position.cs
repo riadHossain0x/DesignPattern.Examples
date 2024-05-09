a6300e8197eef1697479c53bb7890a344ab04067
Riad <riadofficial.me@gmail.com>
2024-05-09 17:50:33 +0600

    memento design pattern

---
+++ Behavioral/Memento/Position.cs
@@ -0,0 +1,39 @@
+﻿namespace Memento;
+
+public class Position
+{
+    private int Cell { get; set; }
+
+    public class Piece
+    {
+        public Position _position = new Position { Cell = 0 };
+
+        public void Move(int n) => _position.Cell = n;
+
+        public Position GetPosition() => new Position { Cell = _position.Cell };
+
+        public void SetPosition(Position p) => _position.Cell = p.Cell;
+    }
+}
+
+public class Player
+{
+    List<Position> positionMemory = new();
+    Position.Piece Piece = new();
+
+    public void GetForward()
+    {
+        var position = Piece.GetPosition();
+        positionMemory.Add(position);
+
+        int n = new Random().Next(1, 6);
+        Piece.Move(n);
+    }
+
+    public void GetBack()
+    {
+        var position = positionMemory.Last();
+        positionMemory.Remove(position);
+        Piece.SetPosition(position);
+    }
+}
